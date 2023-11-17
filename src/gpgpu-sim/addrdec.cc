8ad1615582f2e94008b6f1c8ee34630375af1179
Tim Lühnen <tim.luehnen@tuhh.de>
2023-11-17 13:13:59 +0100

    Formating

--- src/gpgpu-sim/addrdec.cc
+++ src/gpgpu-sim/addrdec.cc
@@ -582,13 +582,13 @@ unsigned next_powerOf2(unsigned n) {
   // decrement `n` (to handle the case when `n` itself
   // is a power of 2)
   n = n - 1;
- 
+
   // initialize result by 2
   int k = 2;
- 
-    // double `k` and divide `n` in half till it becomes 0
+
+  // double `k` and divide `n` in half till it becomes 0
   while (n >>= 1) {
-    k = k << 1;    // double `k`
+    k = k << 1;  // double `k`
   }
 
   return k;
